e1c7feea2ca1692f19e716874f875be6365e3b07
Germán Valencia Vargas <gvalenncia@gmail.com>
2019-01-24 21:58:46 -0500

    Grouping and reducing the input

--- src/group.js
+++ src/group.js
@@ -1,5 +1,16 @@
-module.exports.groupBy = function (input) {
+const R = require('ramda');
+
+module.exports.groupListByDate = function (input) {
     if(input && input.length>0) {
+        let sInput = R.sortWith([R.ascend(R.prop(0)), R.ascend(R.prop(1))], input);
+        let gsInput = R.groupWith((a, b) => a[0] === b[0], sInput);
+
+        const groupDates = (acc, array) => acc.concat({stripe: array[0][1], value: array[0][3]});
+        const fnKey = (array) => array[0][0];
+        let rgsSinput = R.reduceBy(groupDates, [], fnKey, gsInput);
+
+        console.log(rgsSinput);
+
         return {};
     } else {
         return {};
--- test/group.test.js
+++ test/group.test.js
@@ -1,7 +1,16 @@
 const group = require('../src/group')
 
 test('should return an empty object when getting empty list as parameter', () => {
-    let outcome = group.groupBy([]);
+    let outcome = group.groupListByDate([]);
+    expect(outcome).toEqual({});
+});
+
+test('should return an empty object when getting empty list as parameter', () => {
+    let outcome = group.groupListByDate(
+        [["2018-12-01", "AM", "ID123", 5000],
+            ["2018-12-01", "AM", "ID545", 7000],
+            ["2018-12-02", "AM", "ID545", 7000],
+            ["2018-12-01", "PM", "ID545", 3000]]);
     expect(outcome).toEqual({});
 });
 
