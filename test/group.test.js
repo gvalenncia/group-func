04cb03e6681427f77399cc1a8472976a25b2aab8
Germán Valencia Vargas <gvalenncia@gmail.com>
2019-01-25 10:41:31 -0500

    Mapping and reducing to sum the stripes

--- test/group.test.js
+++ test/group.test.js
@@ -1,6 +1,26 @@
 const group = require('./../src/group')
 
-test('should return an empty object when getting empty list as parameter', () => {
-    let outcome = group.groupBy([])
+test('should return an empty object when getting an empty list as parameter', () => {
+
+    //when
+    let outcome = group.groupByDateAndStripe([]);
+
+    //then
     expect(outcome).toEqual({});
+});
+
+
+test('should return sorted object with 2 keys when getting an array of 4 lists', () => {
+    //given
+    let input = [["2018-12-01", "AM", "ID123", 5000],
+        ["2018-12-02", "AM", "ID545", 7000],
+        ["2018-12-01", "AM", "ID545", 7000],
+        ["2018-12-01", "PM", "ID545", 3000]];
+    let expected = {"2018-12-01":{"AM":12000, "PM":3000}, "2018-12-02":{"AM":7000}};
+
+    //when
+    let outcome = group.groupByDateAndStripe( input );
+
+    //then
+    expect( outcome ).toEqual( expected );
 });
